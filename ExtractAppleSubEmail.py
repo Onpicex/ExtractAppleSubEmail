2e893114c4d636e57ec345e9b5fce8e93564c7d7
Onpicex <72106979+Onpicex@users.noreply.github.com>
2023-05-18 12:32:02 +0800

    Update ExtractAppleSubEmail.py

--- ExtractAppleSubEmail.py
+++ ExtractAppleSubEmail.py
@@ -1,27 +1,9 @@
-# import re
-# import pandas as pd
-
-# # 使用 'r' 参数和 'utf-8' 编码打开文件以便读取
-# with open('input.txt', 'r', encoding='utf-8') as f:
-#     text = f.read()
-
-# pattern = r'[a-zA-Z0-9_.+-]+@icloud\.com'
-# matches = re.findall(pattern, text)
-
-# # 去除重复匹配项
-# unique_matches = list(set(matches))
-
-# # 将匹配结果转换为 DataFrame
-# df = pd.DataFrame(unique_matches, columns=['Email'])
-
-# # 将 DataFrame 写入 Excel 文件
-# df.to_excel("output.xlsx", index=False)
-
-
 import re
+import pandas as pd
 
-# 从对话框输入数据
-text = input("Please enter the text: ")
+# 使用 'r' 参数和 'utf-8' 编码打开文件以便读取
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
 
 pattern = r'[a-zA-Z0-9_.+-]+@icloud\.com'
 matches = re.findall(pattern, text)
@@ -29,6 +11,8 @@ matches = re.findall(pattern, text)
 # 去除重复匹配项
 unique_matches = list(set(matches))
 
-# 在对话框中输出每个匹配的电子邮件地址
-for email in unique_matches:
-    print(email)
+# 将匹配结果转换为 DataFrame
+df = pd.DataFrame(unique_matches, columns=['Email'])
+
+# 将 DataFrame 写入 Excel 文件
+df.to_excel("output.xlsx", index=False)
